40111a1aebc97df0745c83c91ecf3ea94962989e
Syed-Muhammad-Hussnain-Raza <hussnain.fyp26@hazelsoft.org>
2026-07-02 22:05:15 +0500

    constant terms extracted

---
+++ Constants/MembershipTypes.cs
@@ -0,0 +1,14 @@
+namespace MyAssignment.Constants
+{
+    public static class MembershipTypes
+    {
+        public const string Basic = "Basic";
+        public const string Premium = "Premium";
+
+        public static readonly string[] AllowedTypes =
+        {
+            Basic,
+            Premium
+        };
+    }
+}
