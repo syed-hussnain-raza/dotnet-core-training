9bdf50708a335d300de3cb6e98650cfb03e4b9c4
Syed-Muhammad-Hussnain-Raza <hussnain.fyp26@hazelsoft.org>
2026-07-02 22:12:36 +0500

    Program.cs Updated

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 // register controllers so app look for controller class
 builder.Services.AddControllers();
 
-//  finalize registeration and build the runnable app
+// finalize registeration and build the runnable app
 var app = builder.Build();
 
-// 
+// map controller endpoints
 app.MapControllers();
 
-//  start web server and listen for incoming requests
+// start web server and listen for incoming requests
 app.Run();
