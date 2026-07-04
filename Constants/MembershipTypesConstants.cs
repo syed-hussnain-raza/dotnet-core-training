3936c012aa250c460fc7e9864cb32f0e5812796d
Syed-Muhammad-Hussnain-Raza <hussnain.fyp26@hazelsoft.org>
2026-07-05 02:10:15 +0500

    rename files

---
+++ Constants/MembershipTypesConstants.cs
@@ -0,0 +1,11 @@
+namespace MyAssignment.Constants
+{
+    /// <summary>
+    /// Centralized collection of Membership Type.
+    /// </summary>
+    public static class MembershipTypesConstants
+    {
+        public const string Basic = "Basic";
+        public const string Premium = "Premium";
+    }
+}
--- Constants/MessagesConstants.cs
+++ Constants/MessagesConstants.cs
@@ -3,16 +3,14 @@ namespace MyAssignment.Constants
     /// <summary>
     /// Centralized collection of response messages used across the User API.
     /// </summary>
-    public static class UserMessages
+    public static class MessagesConstants
     {
-        public const string UsersFetched = "Fetching all users.";
-        public const string UserFetched = "User fetched successfully.";
-        public const string UserCreated = "User created successfully.";
-        public const string UserUpdated = "User updated successfully.";
-        public const string UserDeleted = "User deleted successfully.";
         public const string UserNotFound = "User not found.";
-        public const string UserNotFoundForUpdate = "User not found for update.";
-        public const string ValidationFailed = "FullName, Email and PhoneNumber are required.";
+        public const string UserCreated = "User created successfully.";
+        public const string UserDeleted = "User deleted successfully.";
+        public const string UserUpdated = "User updated successfully.";
+        public const string UserFetched = "User fetched successfully.";
+        public const string UsersFetched = "Fetching all users.";
         public const string InvalidEmailFormat = "Email format is invalid.";
         public const string InvalidPhoneFormat = "Phone number format is invalid.";
         public const string InvalidMembershipType = "MembershipType must be either 'Basic' or 'Premium'.";
