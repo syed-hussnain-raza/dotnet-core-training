d6cba90909f1239223ce5dbd9e8bef1c71ec9a0e
Syed-Muhammad-Hussnain-Raza <hussnain.fyp26@hazelsoft.org>
2026-07-09 15:29:55 +0500

    RegisterDto.cs Updated

--- Dtos/RegisterDto.cs
+++ Dtos/RegisterDto.cs
@@ -12,7 +12,6 @@ namespace MyAssignment.Dtos
         public string Email { get; set; } = string.Empty;
 
         [Required]
-        [MinLength(6)]
         public string Password { get; set; } = string.Empty;
     }
 }
