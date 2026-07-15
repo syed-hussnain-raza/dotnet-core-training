e62f21910b2904387d32fa2a3b1d777e56014b21
Syed-Muhammad-Hussnain-Raza <hussnain.fyp26@hazelsoft.org>
2026-07-16 00:00:33 +0500

    migrations added

--- Migrations/20260715175614_MergeUserIdentity.Designer.cs
+++ Migrations/20260715175614_MergeUserIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyAssignment.Data;
 
@@ -11,9 +12,11 @@ using MyAssignment.Data;
 namespace MyAssignment.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260715175614_MergeUserIdentity")]
+    partial class MergeUserIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -172,8 +175,8 @@ namespace MyAssignment.Migrations
                         .IsConcurrencyToken()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<DateOnly>("DateOfBirth")
-                        .HasColumnType("date");
+                    b.Property<DateTime?>("DateOfBirth")
+                        .HasColumnType("datetime2");
 
                     b.Property<string>("Email")
                         .IsRequired()
